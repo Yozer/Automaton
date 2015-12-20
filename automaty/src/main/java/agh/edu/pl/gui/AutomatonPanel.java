b38c695cf0b731e0123c8510c453924a4659b6c5
Dominik Baran <dominik.baran7@gmail.com>
2015-12-20 15:43:28 +0100

    Fixed border

--- automaty/src/main/java/agh/edu/pl/gui/AutomatonPanel.java
+++ automaty/src/main/java/agh/edu/pl/gui/AutomatonPanel.java
@@ -54,41 +54,35 @@ public class AutomatonPanel extends JPanel
 
         this.scale = scale;
         // create border if cellsize is greater or equal 4
-        synchronized (LOCKER)
+
+        if (scale >= 2)
         {
-            if (bufferedImageBorder != null)
+            //scale = 3;
+            bufferedImageBorder = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
+            Graphics2D graphics2D = bufferedImageBorder.createGraphics();
+            graphics2D.setColor(new Color(0, true));
+            graphics2D.fillRect(0, 0, getWidth(), getHeight());
+
+            graphics2D.setPaint(Color.BLACK);
+            graphics2D.setStroke(new BasicStroke(1));
+
+            int width = (int) (getWidth() / scale);
+            int height = (int) (getHeight() / scale);
+            int scaleInt = (int) scale;
+
+            for (int x = 1; x < width; x++)
             {
-
-                if (scale >= 2)
-                {
-                    //scale = 3;
-                    bufferedImageBorder = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
-                    Graphics2D graphics2D = bufferedImageBorder.createGraphics();
-                    graphics2D.setColor(new Color(0, true));
-                    graphics2D.fillRect(0, 0, getWidth(), getHeight());
-
-                    graphics2D.setPaint(Color.BLACK);
-                    graphics2D.setStroke(new BasicStroke(1));
-
-                    int width = (int) (getWidth() / scale);
-                    int height = (int) (getHeight() / scale);
-                    int scaleInt = (int) scale;
-
-                    for (int x = 1; x < width; x++)
-                    {
-                        graphics2D.drawLine(scaleInt * x, 0, scaleInt * x, getHeight());
-                    }
-                    for (int y = 1; y < height; y++)
-                    {
-                        graphics2D.drawLine(0, scaleInt * y, getWidth(), scaleInt * y);
-                    }
-
-                    graphics2D.dispose();
-                } else
-                {
-                    bufferedImageBorder = null;
-                }
+                graphics2D.drawLine(scaleInt * x, 0, scaleInt * x, getHeight());
             }
+            for (int y = 1; y < height; y++)
+            {
+                graphics2D.drawLine(0, scaleInt * y, getWidth(), scaleInt * y);
+            }
+
+            graphics2D.dispose();
+        } else
+        {
+            bufferedImageBorder = null;
         }
     }
 
@@ -98,14 +92,11 @@ public class AutomatonPanel extends JPanel
     }
     public void createBufferedImage(int width, int height)
     {
-        synchronized (LOCKER)
-        {
-            if (bufferedImage != null && bufferedImage.getWidth() == width && bufferedImage.getHeight() == height)
-                return;
+        if (bufferedImage != null && bufferedImage.getWidth() == width && bufferedImage.getHeight() == height)
+            return;
 
-            bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-            pixels = ((DataBufferInt) bufferedImage.getRaster().getDataBuffer()).getData();
-        }
+        bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        pixels = ((DataBufferInt) bufferedImage.getRaster().getDataBuffer()).getData();
     }
 }
 
